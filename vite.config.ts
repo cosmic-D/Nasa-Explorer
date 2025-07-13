d74a3e2cd554d7a314ff83b57227de9bf0802d5c
optistdev <dskok13371@gmail.com>
2025-07-13 07:59:36 +0200

    update backend

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://65.109.127.159:5000',
         changeOrigin: true,
         secure: false,
       },
