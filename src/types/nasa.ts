c42d390e78429bded0e763a430b09cd5122b70ff
cosmic-D <dskok13371@gmail.com>
2025-07-17 18:29:21 +0200

    update the marstype

--- src/types/nasa.ts
+++ src/types/nasa.ts
@@ -38,17 +38,22 @@ export interface MarsRoverResponse {
 
 export interface MarsRover {
   id: number;
-  name: string;
-  landing_date: string;
-  launch_date: string;
-  status: string;
-  max_sol: number;
-  max_date: string;
-  total_photos: number;
-  cameras: Array<{
+  sol: number;
+  camera: {
+    id: number;
     name: string;
+    rover_id: number;
     full_name: string;
-  }>;
+  };
+  img_src: string;
+  earth_date: string;
+  rover: {
+    id: number;
+    name: string;
+    landing_date: string;
+    launch_date: string;
+    status: string;
+  };
 }
 
 export interface MarsRoversResponse {
