f3d33eb54e81a9e3bf6e843189f2313c991423af
cosmic-D <dskok13371@gmail.com>
2025-07-17 18:39:39 +0200

    update the issue

--- src/types/nasa.ts
+++ src/types/nasa.ts
@@ -38,22 +38,17 @@ export interface MarsRoverResponse {
 
 export interface MarsRover {
   id: number;
-  sol: number;
-  camera: {
-    id: number;
+  name: string;
+  landing_date: string;
+  launch_date: string;
+  status: string;
+  max_sol: number;
+  max_date: string;
+  total_photos: number;
+  cameras: Array<{
     name: string;
-    rover_id: number;
     full_name: string;
-  };
-  img_src: string;
-  earth_date: string;
-  rover: {
-    id: number;
-    name: string;
-    landing_date: string;
-    launch_date: string;
-    status: string;
-  };
+  }>;
 }
 
 export interface MarsRoversResponse {
